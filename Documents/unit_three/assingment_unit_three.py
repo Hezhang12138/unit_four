661a1e9d79bb13132d5cd2070cd881202bbf2050
Frank <frank@iMac3.local>
2018-10-01 09:32:49 -0400

    finish the area of the rectangle.

--- Documents/unit_three/assingment_unit_three.py
+++ Documents/unit_three/assingment_unit_three.py
@@ -4,30 +4,49 @@ def printInstruction():
     print("This program calculates the serfacearea of the rectangle")
     print("Please enter the data of the rectangle")
 
-def getUsersinput():
-    Length = input("Enter Length:")
-    return float(Length)
-    width = input("Enter Width:")
-    return float(Width)
+
+def the_area_of_the_rectangle(length, width):
+    area = length * width
+    return area
+
+
+def getLength():
+    length = input("Enter Length:")
+    return length
+
+
+def getWidth():
+    width = input("Enter With:")
+    return width
+
+
+def getHeight():
     height = input("Enter Height:")
-    return float(Height)
+    return height
+
 
 def sidearea1():
-    sidearea1 = Length * Width
+    sidearea1 = length * getWidth()
+
 
 def sidearea2():
+    sidearea2 = getWidth() * getHeight()
 
 
 def sidearea3():
+    sidearea3 = getLength() * getHeight()
 
 
 def surfacearea():
+    surfacearea = sidearea1() + sidearea2() + sidearea3()
 
 
 
 def main():
     printInstruction()
-    getUsersinput()
+    length = getLength()
+    width = getWidth()
+    height = getHeight()
     sidearea1()
     sidearea2()
     sidearea3()
