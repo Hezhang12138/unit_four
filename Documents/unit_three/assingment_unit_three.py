4693d1dcf0ba2a1a4d561cb8de8a7801cb4c575d
Frank <frank@iMac3.local>
2018-10-01 10:11:53 -0400

    finish the third step

--- Documents/unit_three/assingment_unit_three.py
+++ Documents/unit_three/assingment_unit_three.py
@@ -1,8 +1,27 @@
+# Frank Zhang
+# 10/1/18
+# this program helps me a lot to understand what computer science is. Before this I did not figure out what it is.
+# Unless I know what I should do.
 
 
 def printInstruction():
     print("This program calculates the serfacearea of the rectangle")
     print("Please enter the data of the rectangle")
+
+
+def getLength():
+    length = input("Enter Length:")
+    return float(length)
+
+
+def getWidth():
+    width = input("Enter Width:")
+    return float(width)
+
+
+def getHeight():
+    height = input("Enter Height:")
+    return float(height)
 
 
 def the_area_of_the_rectangle(length, width):
@@ -14,21 +33,7 @@ def the_surface_area_of_the_rectangle(length, width, height):
     front = length * height
     side = width * height
     top = length * width
-
-
-def getLength():
-    length = input("Enter Length:")
-    return length
-
-
-def getWidth():
-    width = input("Enter Width:")
-    return width
-
-
-def getHeight():
-    height = input("Enter Height:")
-    return height
+    return 2 * (front + side + top)
 
 
 def main():
@@ -36,7 +41,8 @@ def main():
     length = getLength()
     width = getWidth()
     height = getHeight()
-    the_surface_area_of_the_rectangle()
-    printresult()
+    surfacearea = the_surface_area_of_the_rectangle(length, width, height)
+    print("the surface area is", surfacearea)
+
 
 main()
