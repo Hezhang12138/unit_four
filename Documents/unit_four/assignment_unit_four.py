eb55acc6244c3f672d76465f403e60e33b1e7c12
Frank <frank@iMac3.local>
2018-10-04 12:47:26 -0400

    begin the assignment.

---
+++ Documents/unit_four/assignment_unit_four.py
@@ -0,0 +1,14 @@
+# assignment_unit_four.py
+# by Frank Zhang
+# Last Modified
+#
+#
+
+def main():
+    draw_a_card(1, 10)
+    total()
+
+    
+def draw_a_card():
+    number = draw_a_card(1, 10)
+    print(number)
