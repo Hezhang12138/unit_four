1ddaabd2d7ee457f3927df0f556689aa81b39fc7
Frank <frank@iMac3.local>
2018-10-05 14:13:24 -0400

    I am half way finished now.

--- Documents/unit_four/assignment_unit_four.py
+++ Documents/unit_four/assignment_unit_four.py
@@ -3,12 +3,21 @@
 # Last Modified
 #
 #
+import random
+
+
+def draw_a_card(random):
+    number = random.randint(1, 10)
+    return number
+
 
 def main():
-    draw_a_card(1, 10)
-    total()
+    card1 = draw_a_card(random)
+    card2 = draw_a_card(random)
+    total1 = card1 + card2
+    print("You drew a", card1, "and a", card2)
+    print("Your total is", total1)
+    print("Would you like another card")
 
-    
-def draw_a_card():
-    number = draw_a_card(1, 10)
-    print(number)
+
+main()
