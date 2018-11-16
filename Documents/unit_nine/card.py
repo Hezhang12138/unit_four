83f42b7d384e93c533786cf3de1b1dbaa267267c
Frank <frank@iMac3.local>
2018-11-16 13:55:00 -0500

    create deck class

--- Documents/unit_nine/card.py
+++ Documents/unit_nine/card.py
@@ -5,4 +5,14 @@ class Card:
         self.rank = rank
         self.suit = suit
 
-    
+
+    def compared_to(self, other_card):
+        if self.rank > other_card.rank:
+            return self
+        elif other_card.rank > self.rank:
+            return other_card
+        else:
+            if self.suit > other_card.suit:
+                return self
+            else:
+                return other_card
