aa92edab94d88efaae11c2b6bbf489e38e8f5c60
Frank <frank@iMac3.local>
2018-11-30 11:18:36 -0500

    partialy done

--- Documents/unit_nine/assignment_unit_nine.py
+++ Documents/unit_nine/assignment_unit_nine.py
@@ -3,7 +3,24 @@ import deck
 
 
 def deal_cards():
-    list_one = []
-    list_two = []
-    
+    deck1 = deck.Deck()
+    deck.shuffle()
+    list1 = []
+    list2 = []
+    for x in range(5):
+        card1 = deck1.draw_a_card()
+        card2 = deck1.draw_a_card()
+        list1.append(card1)
+        list2.append(card2)
+    return list1, list2
 
+
+def compare_cards():
+    result1 = 0
+    result2 = 0
+
+
+def main():
+    player1, player2 = deal_cards()
+    compared_to(list1, list2)
+    
